b501a4e70ede1b5dd7967e722a9b6cf16b4fe663
eduardoLemes <eduardo_lsf@outlook.com>
2022-08-10 01:40:48 -0300

    Cria interface verificaConta para pagar boleto

--- main.go
+++ main.go
@@ -6,10 +6,24 @@ import (
 	"github.com/eduardo-lemes/goLang/contas"
 )
 
+func PagarBoleto(conta verificarConta, valorBoleto float64) {
+	conta.Sacar(valorBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaEdu := contas.ContaPoupanca{}
 	contaEdu.Depositar(100)
-	contaEdu.Sacar(50)
+	PagarBoleto(&contaEdu, 50)
 
 	fmt.Println(contaEdu.MostraSaldo())
+
+	contaLemes := contas.ContaCorrente{}
+	contaLemes.Depositar(300)
+	PagarBoleto(&contaLemes, 100)
+
+	fmt.Println(contaLemes.MostraSaldo())
 }
